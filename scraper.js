73c0d0f7a6e548c5fb39df91ee7aef47bec4ad2f
juandjara <juanorigami@gmail.com>
2017-05-29 11:08:57 +0200

    feat: withSlug function

--- scraper.js
+++ scraper.js
@@ -4,9 +4,16 @@ const base_url = "https://ww1.gogoanime.io";
 // TODO: Control concurrency and response time with
 // npm.im/bottleneck
 
+function withSlug(elem) {
+  const slugIndex = elem.url.lastIndexOf('/');
+  const slug = elem.url.substring(slugIndex + 1);
+  elem.slug = slug;
+  return elem;
+}
+
 module.exports = {
   search(query) {
-    return scraper.Anime.fromName(query)
+    return scraper.Anime.search(query).then(res => res.map(withSlug))
   },
   getShow(slug) {
     return scraper.Anime.fromUrl(`${base_url}/category/${slug}`)
